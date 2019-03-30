5730d8b963e5312f1fe1af34eda9280162d0e563
Reese <quattro004@gmail.com>
2019-03-30 06:58:34 -0700

    Getting a list of recipes to display.

---
+++ RecipeUIClassLib/Areas/Recipes/Models/IndexModel.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeUIClassLib.Areas.Recipes.Models;
+using RecipeUIClassLib.Areas.Recipes.Services;
+
+namespace RecipeUIClassLib.Areas.Recipes.Models
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IRecipeService _recipeService;
+
+        public IEnumerable<RecipeViewModel> Recipes { get; set; }
+
+        public IndexModel(IRecipeService recipeService)
+        {
+            _recipeService = recipeService ?? throw new ArgumentNullException(nameof(recipeService));
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Recipes = await _recipeService.GetRecipes();
+
+            return Page();
+        }
+    }
+}
