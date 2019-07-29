970ce6e0d582e15aca331f4179bcd4407b376a2d
Reese <quattro004@gmail.com>
2019-07-29 07:02:31 -0700

    Getting solid auth working Moved files in wwwroot to src, getting webpack working

---
+++ RecipiecesWeb/src/js/site.js
@@ -0,0 +1,31 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+'use strict';
+const auth = require('solid-auth-client');
+const $rdf = require('rdflib')
+var Recipieces = Recipieces || {};
+
+$('#login button').click(popupLogin());
+
+Recipieces.auth = (function() {
+    async function popupLogin() {
+        let session = await auth.currentSession();
+        let popupUri = '~/src/recipieces-login-popup.html';
+        if (!session) {
+            session = await auth.popupLogin({ popupUri });
+        }
+        alert('Logged in as ${session.webId}');
+    }
+
+    // Update components to match the user's login status
+    auth.trackSession(session => {
+        const loggedIn = !!session;
+        // $('#login').toggle(!loggedIn);
+        // $('#logout').toggle(loggedIn);
+        $('#user').text(session && session.webId);
+    });
+
+    return {
+        popupLogin: popupLogin
+    };
+  }());
